48d47d3c02a0b9b1403d051b672754ae98498b79
freender <40869255+freender@users.noreply.github.com>
2024-05-29 01:04:59 -0400

    Update modules.py

--- src/modules/modules.py
+++ src/modules/modules.py
@@ -1,5 +1,7 @@
 import os
 import re
+from uptime_kuma_api import UptimeKumaApi
+    
 
 COMMANDS = {
 'start_silent': 'Start Silent MW',
@@ -13,3 +15,23 @@ COMMANDS = {
 def is_command(string):
     pattern = r"^\/.*$"
     return bool(re.match(pattern, string))
+
+def start_mw():
+    KUMA_HOST = os.environ['KUMA_HOST']
+    KUMA_LOGIN = os.environ['KUMA_LOGIN']
+    KUMA_PASSWORD = os.environ['KUMA_PASSWORD']
+    KUMA_MW_ID = os.environ['KUMA_MW_ID']
+    api = UptimeKumaApi(KUMA_HOST)
+    api.login(KUMA_LOGIN,KUMA_PASSWORD)
+    api.resume_maintenance(KUMA_MW_ID)
+    api.disconnect()
+
+def stop_mw():
+    KUMA_HOST = os.environ['KUMA_HOST']
+    KUMA_LOGIN = os.environ['KUMA_LOGIN']
+    KUMA_PASSWORD = os.environ['KUMA_PASSWORD']
+    KUMA_MW_ID = os.environ['KUMA_MW_ID']
+    api = UptimeKumaApi(KUMA_HOST)
+    api.login(KUMA_LOGIN,KUMA_PASSWORD)
+    api.pause_maintenance(KUMA_MW_ID)
+    api.disconnect()
